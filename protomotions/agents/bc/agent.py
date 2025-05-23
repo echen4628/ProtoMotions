be4386176880c87a89d65e116c054237e80fc723
echen4628 <echen4628@gmail.com>
2025-05-23 03:06:32 +0000

    seemingly working optimize function

--- protomotions/agents/bc/agent.py
+++ protomotions/agents/bc/agent.py
@@ -41,6 +41,7 @@ class BC:
 
         self.current_epoch = 0
         self.step_count = 0
+        self.num_mini_epochs: int = config.num_mini_epochs
         self.fit_start_time = None
         self.best_evaluated_score = None
         self.episode_reward_meter = AverageMeter(1, 100).to(self.device)
@@ -77,7 +78,7 @@ class BC:
             checkpoint = Path(checkpoint).resolve()
             print(f"Loading model from checkpoint as actor: {checkpoint}")
             state_dict = torch.load(checkpoint, map_location=self.device)
-            self.load_parameters(state_dict)
+            self.load_actor_parameters(state_dict)
             
             env_checkpoint = checkpoint.resolve().parent / f"env_{self.fabric.global_rank}.ckpt"
             if env_checkpoint.exists():
@@ -133,6 +134,18 @@ class BC:
         self.experience_buffer.register_key("rewards")
         self.experience_buffer.register_key("dones", dtype=torch.long)
         self.experience_buffer.register_key("neglogp")
+
+        if self.config.get("extra_inputs", None) is not None:
+            obs = self.env.get_obs()
+            for key in self.config.extra_inputs.keys():
+                assert (
+                    key in obs
+                ), f"Key {key} not found in obs returned from env: {obs.keys()}"
+                env_tensor = obs[key]
+                shape = env_tensor.shape
+                dtype = env_tensor.dtype
+                self.experience_buffer.register_key(key, shape=shape[1:], dtype=dtype)
+
         initial_expertdata = None
 
         while self.current_epoch < self.config.max_epochs:
@@ -151,28 +164,10 @@ class BC:
                     )
 
             # training_log_dict["epoch"] = self.current_epoch
-            dataset = self.experience_buffer.make_dict()
+            # dataset = self.experience_buffer.make_dict()
 
-            # # training/optimization loop
-            # for batch_idx in track(
-            #     range(self.max_num_batches()),
-            #     description=f"Epoch {self.current_epoch}, training...",
-            # ):
-            #     dataset_idx = batch_idx % len(dataset)
-            #     if dataset_idx == 0 and batch_idx != 0 and dataset.do_shuffle:
-            #         dataset.shuffle()            
-            #     batch_dict = dataset[dataset_idx]
-
-            #     # Go over all keys in obs and check if any has nans
-            #     for key in batch_dict.keys():
-            #         has_nan = False
-            #         if torch.isnan(batch_dict[key]).any():
-            #             has_nan = True
-            #             print(f"NaN in {key}: {batch_dict[key]}")
-            #         if has_nan:
-            #             raise ValueError("NaN in training")
-                    
-            #     self.model_step(batch_dict)
+            self.optimize_model()
+            print("yay")
 
             self.current_epoch += 1
             self.fabric.call("after_train", self)
@@ -206,7 +201,7 @@ class BC:
         #         envsteps_this_batch = sum([utils.get_pathlength(path) for path in paths])
         # else:
         print("\nCollecting data to be used for training...")
-        eval_batch_size = 2
+        # eval_batch_size = 2
         motion_tag = list(self.experts.keys())[0]
         print(f"\nUsing {motion_tag} expert")
         self.sample_trajectories(self.num_steps, motion_tag, use_expert)
@@ -220,8 +215,10 @@ class BC:
         for step in range(min_timesteps_per_batch):
             obs = self.handle_reset(done_indices)
             self.experience_buffer.update_data("self_obs", step, obs["self_obs"])
+            if self.config.get("extra_inputs", None) is not None:
+                for key in self.config.extra_inputs:
+                    self.experience_buffer.update_data(key, step, obs[key])
 
-            # TODO: ignore the value cause we dont have critic
             action, neglogp, _ = self.model.get_action_and_value(obs)
             if use_expert:
                 expert_action, _, _ = self.experts[motion_tag].get_action_and_value(obs)
@@ -266,6 +263,121 @@ class BC:
 
     def get_step_count_increment(self):
         return self.env.num_envs * self.fabric.world_size
+
+
+    # -----------------------------
+    # Optimization
+    # -----------------------------
+    def optimize_model(self) -> Dict:
+        # training/optimization loop
+
+        dataset = self.process_dataset(self.experience_buffer.make_dict())
+        self.train()
+        training_log_dict = {}
+
+        for batch_idx in track(
+            range(self.max_num_batches()),
+            description=f"Epoch {self.current_epoch}, training...",
+        ):
+            iter_log_dict = {}
+            dataset_idx = batch_idx % len(dataset)
+            if dataset_idx == 0 and batch_idx != 0 and dataset.do_shuffle:
+                dataset.shuffle()            
+            batch_dict = dataset[dataset_idx]
+
+            # Go over all keys in obs and check if any has nans
+            for key in batch_dict.keys():
+                has_nan = False
+                if torch.isnan(batch_dict[key]).any():
+                    has_nan = True
+                    print(f"NaN in {key}: {batch_dict[key]}")
+                if has_nan:
+                    raise ValueError("NaN in training")
+   
+            loss_dict = self.model_step(batch_dict)
+            iter_log_dict.update(loss_dict)
+
+            for k, v in iter_log_dict.items():
+                if k in training_log_dict:
+                    training_log_dict[k][0] += v
+                    training_log_dict[k][1] += 1
+                else:
+                    training_log_dict[k] = [v, 1]
+
+        for k, v in training_log_dict.items():
+            training_log_dict[k] = v[0] / v[1]
+
+        self.eval()
+        return training_log_dict
+        
+        
+
+
+        # for batch_idx in track(
+        #     range(self.max_num_batches()),
+        #     description=f"Epoch {self.current_epoch}, training...",
+        # ):
+        #     iter_log_dict = {}
+        #     dataset_idx = batch_idx % len(dataset)
+
+        #     # Reshuffle dataset at the beginning of each mini epoch if configured.
+        #     if dataset_idx == 0 and batch_idx != 0 and dataset.do_shuffle:
+        #         dataset.shuffle()
+        #     batch_dict = dataset[dataset_idx]
+
+        #     # Check for NaNs in the batch.
+        #     for key in batch_dict.keys():
+        #         if torch.isnan(batch_dict[key]).any():
+        #             print(f"NaN in {key}: {batch_dict[key]}")
+        #             raise ValueError("NaN in training")
+
+        #     # Update actor
+        #     actor_loss, actor_loss_dict = self.actor_step(batch_dict)
+        #     iter_log_dict.update(actor_loss_dict)
+        #     self.actor_optimizer.zero_grad(set_to_none=True)
+        #     self.fabric.backward(actor_loss)
+        #     actor_grad_clip_dict = self.handle_model_grad_clipping(
+        #         self.model._actor, self.actor_optimizer, "actor"
+        #     )
+        #     iter_log_dict.update(actor_grad_clip_dict)
+        #     self.actor_optimizer.step()
+
+        #     # Update critic
+        #     critic_loss, critic_loss_dict = self.critic_step(batch_dict)
+        #     iter_log_dict.update(critic_loss_dict)
+        #     self.critic_optimizer.zero_grad(set_to_none=True)
+        #     self.fabric.backward(critic_loss)
+        #     critic_grad_clip_dict = self.handle_model_grad_clipping(
+        #         self.model._critic, self.critic_optimizer, "critic"
+        #     )
+        #     iter_log_dict.update(critic_grad_clip_dict)
+        #     self.critic_optimizer.step()
+
+        #     # Extra optimization steps if needed.
+        #     extra_opt_steps_dict = self.extra_optimization_steps(batch_dict, batch_idx)
+        #     iter_log_dict.update(extra_opt_steps_dict)
+
+        #     for k, v in iter_log_dict.items():
+        #         if k in training_log_dict:
+        #             training_log_dict[k][0] += v
+        #             training_log_dict[k][1] += 1
+        #         else:
+        #             training_log_dict[k] = [v, 1]
+
+        # for k, v in training_log_dict.items():
+        #     training_log_dict[k] = v[0] / v[1]
+
+        # self.eval()
+        # return training_log_dict
+
+    # -----------------------------
+    # Helper Functions
+    # -----------------------------
+    def eval(self):
+        self.model.eval()
+
+    def train(self):
+        self.model.train()
     
     def model_step(self, batch_dict):
         """
@@ -273,16 +385,33 @@ class BC:
         """
         # Get policy's action distribution
         dist = self.model._actor(batch_dict)
-        expert_actions = batch_dict["expert_actions"]
-        bc_loss = torch.square(dist.mean - expert_actions).mean()
+        # expert_actions = batch_dict["actions"]
+        # loss = torch.square(dist.mean - expert_actions).mean()
 
-        # logstd = self.model._actor.logstd
-        # std = torch.exp(logstd)
-        # neglogp = self.model.neglogp(batch_dict["actions"], dist.mean, std, logstd)
-        # loss = neglogp.mean()    
+        logstd = self.model._actor.logstd
+        std = torch.exp(logstd)
+        neglogp = self.model.neglogp(batch_dict["actions"], dist.mean, std, logstd)
+        loss = neglogp.mean()    
         
-        self.optimizer.zero_grad()
-        self.fabric.backward(bc_loss)
-        self.optimizer.step()
+        self.actor_optimizer.zero_grad(set_to_none=True)
+        self.fabric.backward(loss)
+        self.actor_optimizer.step()
         
-        return {"model/bc_loss": bc_loss.detach().item()}
+        return {"model/loss": loss.detach().item()}
+
+    @torch.no_grad()
+    def process_dataset(self, dataset):
+        # if self.config.normalize_values:
+        #     self.running_val_norm.update(dataset["values"])
+        #     self.running_val_norm.update(dataset["returns"])
+
+        #     dataset["values"] = self.running_val_norm.normalize(dataset["values"])
+        #     dataset["returns"] = self.running_val_norm.normalize(dataset["returns"])
+
+        dataset = DictDataset(self.config.batch_size, dataset, shuffle=True)
+        return dataset
+
+    def max_num_batches(self):
+        return math.ceil(
+            self.num_envs * self.num_steps * self.num_mini_epochs / self.config.batch_size
+        )
