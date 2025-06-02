da3864098d5bc7c9f2d272a2af6fdaa8a5f8de91
Ubuntu <ubuntu@ip-172-31-15-143.us-west-2.compute.internal>
2025-06-02 04:31:37 +0000

    Update BC agent

--- protomotions/agents/bc/agent.py
+++ protomotions/agents/bc/agent.py
@@ -423,6 +423,9 @@ class BC:
             all_done_indices = dones.nonzero(as_tuple=False)
             done_indices = all_done_indices.squeeze(-1)
             step += 1
+
+            eval_log_dict, evaluated_score = self.calc_eval_metrics()
+            self.fabric.log_dict(eval_log_dict)
     
     def post_epoch_logging(self, training_log_dict: Dict):
         end_time = time.time()
